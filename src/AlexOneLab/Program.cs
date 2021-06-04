36b5608ec712984ea54a1fe6237639058faee9ff
alexthvest <alexthvest@gmail.com>
2021-06-04 18:29:14 +0300

    Initial commit

---
+++ src/AlexOneLab/Program.cs
@@ -0,0 +1,13 @@
+﻿using Replikit.Adapters.Telegram;
+using Replikit.Core;
+using Replikit.Core.Hosting.Modules;
+
+ReplikitHost.RunModule<ProgramModule>();
+
+public class ProgramModule : IReplikitModule
+{
+    public void ConfigureModules(IModuleCollection modules)
+    {
+        modules.AddTelegram();
+    }
+}
